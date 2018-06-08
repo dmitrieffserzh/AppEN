eb23bc5b329575c07f7027b0d4569f31adf80ba5
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-06-08 18:13:58 +0300

    update

--- resources/views/admin/administrator.blade.php
+++ resources/views/admin/administrator.blade.php
@@ -51,7 +51,7 @@
                 dmitrieffserzh
             </a>
 
-            <a href="#" class="top-elements">
+            <a href="{{ route('logout') }}" class="top-elements">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                     <polyline points="16 17 21 12 16 7"></polyline>
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,10 +5,16 @@
     <form class="form-signin shadow-lg" method="POST" action="{{ route('admin.login.submit') }}">
         @csrf
 
-        <h1>Admin Panel</h1>
-        <label for="email" class="sr-only">{{ __('E-Mail Address') }}</label>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-power">
+            <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+            <line x1="12" y1="2" x2="12" y2="12"></line>
+        </svg>
+
+
+        <h1 class="h1">Панель управления</h1>
+        <label for="email" class="sr-only">{{ __('E-Mail') }}</label>
         <input type="email" id="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-               value="{{ old('email') }}" placeholder="Email address" required="" autofocus="">
+               value="{{ old('email') }}" placeholder="Email" required="" autofocus="">
 
         @if ($errors->has('email'))
             <span class="invalid-feedback">
@@ -16,9 +22,9 @@
             </span>
         @endif
 
-        <label for="password" class="sr-only">{{ __('Password') }}</label>
+        <label for="password" class="sr-only">{{ __('Пароль') }}</label>
         <input type="password" id="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-               name="password" placeholder="Password" required="">
+               name="password" placeholder="Пароль" required="">
 
         @if ($errors->has('password'))
             <span class="invalid-feedback">
@@ -28,9 +34,10 @@
 
         <div class="checkbox mb-3">
             <label>
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Не выходить из системы') }}
             </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">{{ __('Login') }}</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">{{ __('Войти') }}</button>
+        <a href="{{ route('home') }}" class="d-block mt-2 text-primary small">Вернуться на сайт</a>
     </form>
 @endsection
