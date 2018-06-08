cf22b006ce10f2b0f271fe7079037e89aef6ef0e
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-06-08 16:31:25 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,32 @@
 |
 */
 
+
+// FRONTEND ==========================================================================================================//
 Auth::routes();
-
-Route::get('/', 'HomeController@index')->name('home');
-
+Route::get          ('/',                               [ 'as' => 'home', 'uses' => 'HomeController@index' ]);
 
 
 
 // ADMIN PANEL =======================================================================================================//
 
 Route::group([
-	'namespace' => 'Admin',
-	'prefix'    => 'admin',
-	'name'      => 'admin'] , function() {
+	'namespace'     => 'Admin',
+	'prefix'        => 'admin',
+	'name'          => 'admin',
+	/*'middleware'    => ['auth']*/],
 
-	Route::get('/',                                         [ 'as' => 'admin.dashboard', 'uses' => 'AdminController@index' ]);
-	Route::resource('news',         'NewsController',       [ 'as' => 'admin' ]);
-	Route::resource('stories',      'StoryController',      [ 'as' => 'admin' ]);
-	Route::resource('pages',        'PageController',       [ 'as' => 'admin' ]);
-	Route::resource('comments',     'CommentController',    [ 'as' => 'admin' ]);
-	Route::resource('categories',   'CategoryController',   [ 'as' => 'admin' ]);
-	Route::resource('tags',         'TagController',        [ 'as' => 'admin' ]);
+	function() {
 
+	Route::resource('news',       'NewsController',     [ 'as' => 'admin' ]);
+	Route::resource('stories',    'StoryController',    [ 'as' => 'admin' ]);
+	Route::resource('pages',      'PageController',     [ 'as' => 'admin' ]);
+	Route::resource('comments',   'CommentController',  [ 'as' => 'admin' ]);
+	Route::resource('categories', 'CategoryController', [ 'as' => 'admin' ]);
+	Route::resource('tags',       'TagController',      [ 'as' => 'admin' ]);
+	Route::resource('users',      'UserController',     [ 'as' => 'admin' ]);
+	Route::resource('roles',      'RoleController',     [ 'as' => 'admin' ]);
+	Route::get     ('/',                                [ 'as' => 'admin.dashboard', 'uses' => 'AdminController@index' ]);
 
 });
 
